e6881b7a2d8382615688d65994d361063cc6c02b
FMoreiraSouza <fmoreirasouza701@gmail.com>
2026-05-20 17:08:19 -0300

    chore-api: setup initial project structure

---
+++ aircraft-management-api/src/test/java/com/example/aircraft_management_api/AircraftManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aircraft_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
